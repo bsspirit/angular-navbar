6e250ebb7d4f38d651b795f9538a414b07ae34db
bsspirit <bsspirit@163.com>
2014-06-26 23:12:26 +0800

    多级菜单

--- js/app.js
+++ js/app.js
@@ -24,3 +24,23 @@ page2.controller('DemoCtrl', function () {
     // nothing
 });
 
+
+var page4 = angular.module('page4', ['ui.bootstrap', 'ngRoute']);
+
+page4.config(['$routeProvider', '$locationProvider', '$sceProvider', function ($routeProvider, $locationProvider, $sceProvider) {
+    $routeProvider
+        .when('/', {controller: 'DemoCtrl'})
+        .otherwise({redirectTo: '/'});
+    $locationProvider.html5Mode(true);
+}]);
+
+page4.controller('NavbarCtrl', function ($scope,$http,$location) {
+    $http.get("/js/nav.json").success(function(json){
+        $scope.navbar = json;
+    });
+});
+
+page4.controller('DemoCtrl', function () {
+    // nothing
+});
+
